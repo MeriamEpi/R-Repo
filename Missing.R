f0b84c500bdd2aa2ad3209fbecfb300233664beb
‘MeriamEpi’ <mberka1@jh.edu>
2023-11-02 13:53:53 -0400

    what am I even supposed to say

--- Missing.R
+++ Missing.R
@@ -3,7 +3,7 @@ count_missing <- apply(data,2,function(x) sum(is.na(x)))
 sort(count_missing)
 
 # Analysis of COVID experience data missingness
-data_sub <- data %>% select(MARRIED, URB_RUR, MAT_AGE_NAPHSIS, MAT_ED, MAT_RACE, PAY, MAT_WIC, STATE, PTB, LBW, CVEXP_MJB, CVEXP_BILL, CVEXP_DEP, CVEXP_ANX)
+data_sub <- data %>% select(MARRIED, URB_RUR, MAT_AGE_NAPHSIS, MAT_ED, MAT_RACE, PAY, MAT_WIC, STATE, CVEXP_MJB, CVEXP_BILL, CVEXP_DEP, CVEXP_ANX)
 
 data_sub <- data_sub %>% mutate(missing_job = ifelse((is.na(CVEXP_MJB)), 1, 0))
 data_sub <- data_sub %>% mutate(missing_bill = ifelse((is.na(CVEXP_BILL)), 1, 0))
@@ -16,4 +16,15 @@ data_sub %>% count(missing)
 data_sub <- data_sub %>% mutate(missing = ifelse(missing > 0, 1, 0))
 data_sub %>% count(missing)
 
-missing <- data_sub %>% group_by(missing)
+# removing CVEXP variables and creating subsets by missing
+missing_0 <- data_sub %>% filter(missing==0) %>% select(MARRIED, URB_RUR, MAT_AGE_NAPHSIS, MAT_ED, MAT_RACE, PAY, MAT_WIC, STATE, PTB, LBW)
+missing_1 <- data_sub %>% filter(missing==1) %>% select(MARRIED, URB_RUR, MAT_AGE_NAPHSIS, MAT_ED, MAT_RACE, PAY, MAT_WIC, STATE, PTB, LBW)
+
+# tabulating missingness by CVEXP missing
+count_missing_0 <- apply(missing_0,2,function(x) sum(is.na(x)))
+percent_missing_0 <- count_missing_0/nrow(missing_0)
+sort(percent_missing_0)
+
+count_missing_1 <- apply(missing_1,2,function(x) sum(is.na(x)))
+percent_missing_1 <- count_missing_1/nrow(missing_1)
+sort(percent_missing_1)
