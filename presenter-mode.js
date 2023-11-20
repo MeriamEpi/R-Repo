47191db0f3e41c65d3eb120cad5bb9e7b6c9b712
MeriamEpi <144382975+MeriamEpi@users.noreply.github.com>
2023-11-20 11:50:36 -0500

    Delete presenter-mode.js

--- presenter-mode.js
+++
@@ -1,7 +0,0 @@
-<script>
-const urlParams = new URLSearchParams(window.location.search);
-const presentMe = urlParams.get("presentme");
-if (presentMe === "true") {
-  alert("Presenter mode is enabled!");
-}
-</script>
